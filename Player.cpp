5051f3f6551b05d55acd4c6a911c3c00894ba52f
KensyuuEn <k021g1321@g.neec.ac.jp>
2022-11-10 14:28:06 +0900

    BossにPlayerを追加

--- Player.cpp
+++ Player.cpp
@@ -1,13 +1,64 @@
-#include "Player.h"
+#include "TextureManager.h"
+#include <cassert>
+#include<Player.h>
+#include<random>
+
 
 void Player::Initialize()
 {
+	PlayerHp_ = 500;
+	PlayerAttackFlag_ = 0;
+	Select = 0;
+	input_ = Input::GetInstance();
+	std::random_device seed_gen;
+	std::mt19937_64 engine(seed_gen());
+	std::uniform_real_distribution<float>PlayerDamage(50.0f, 100.0f);
+	std::uniform_real_distribution<float>PlayerHeal(150.0f, 200.0f);
+	std::uniform_real_distribution<float>PlayerGuard(0.5f, 0.75f);
+
 }
 
 void Player::Update()
 {
+	if (input_->TriggerKey(DIK_LEFT))
+	{
+		Select -= 1;
+		if (Select < 0)
+		{
+			Select = 0;
+		}
+	}
+	else if (input_->TriggerKey(DIK_RIGHT))
+	{
+		Select += 1;
+		if (Select > 4)
+		{
+			Select = 4;
+		}
+	}
+	if (Select == 0)
+	{
+		PlayerSpeed = 2;
+	}
+	else if (Select == 1)
+	{
+		PlayerSpeed = 3;
+	}
+	else if (Select == 2)
+	{
+		PlayerSpeed = 3;
+	}
+	else if (Select == 4)
+	{
+		PlayerSpeed = 4;
+	}
 }
 
 void Player::Draw()
 {
+}
+
+float Player::Speed()
+{
+	return PlayerSpeed;
 }
--- Player.h
+++ Player.h
@@ -1,11 +1,25 @@
 #pragma once
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
 class Player
 {
+private:
+	int PlayerHp_;
+	int PlayerAttackFlag_;
+	float PlayerSpeed;
+	int Select;
+	Input* input_ = nullptr;
 public:
 	void Initialize();
-
 	void Update();
-
 	void Draw();
+	float Speed();
 };
-
